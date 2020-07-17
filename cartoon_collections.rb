84a6a34ec9e9dd816d1858533c2b5802ae99ddb0
John Easter <john@owltestprep.com>
2020-07-17 16:44:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 
 def find_valid_calls(planeteer_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart"])
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find
+  planeteer_calls.find { |calls| 
     
   end 
     
